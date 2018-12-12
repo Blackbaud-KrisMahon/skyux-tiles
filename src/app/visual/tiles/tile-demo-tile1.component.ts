1f22198d8ea4b14abcbd10c29ac3f019c105c17b
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-12-12 13:58:58 -0500

    Demo for help widget

--- src/app/visual/tiles/tile-demo-tile1.component.ts
+++ src/app/visual/tiles/tile-demo-tile1.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 
+import { HelpWidgetService } from '@blackbaud/skyux-lib-help';
+
 @Component({
   // tslint:disable-next-line
   selector: 'div.tile1',
   templateUrl: './tile-demo-tile1.component.html'
 })
 export class SkyTileDemoTile1Component {
+
+  constructor(private helpService: HelpWidgetService) {}
+
   public tileHelpClick() {
-    alert('tile help clicked');
+    this.helpService.openWidget('sec-adduser.html');
   }
 
   public tileSettingsClick() {
